b33397cd6d8384e582c5f0afe52fa5a6510de5aa
Peter Danilin <peter@Peters-MacBook-Pro-3.local>
2023-10-01 00:36:22 -0400

    Improved backtester test class, simplified main.py

--- main.py
+++ main.py
@@ -3,49 +3,49 @@ from backtesting.backtester import Backtester
 from strategies.moving_average_strategy import MovingAverageStrategy
 from data.data_manager import DataManager
 
-def load_data():
-    data_manager = DataManager()
-    data = data_manager.get_candles('AAPL', '2023-01-01', '2023-12-31', interval='60m')
+class Main:
+    def __init__(self):
+        self.data_manager = DataManager()
+        self.initial_capital = 100000
     
-    strategy = MovingAverageStrategy(tickers=['AAPL'], interval='1d', short_window=2, long_window=3)
-    data_with_signals = strategy.generate_signals(data)
+    def load_data(self, ticker, start_date, end_date, interval='60m'):
+        return self.data_manager.get_candles(ticker, start_date, end_date, interval)
     
-    return data_with_signals
-
-
-def optimize_strategy(data):
-    best_result = None
-    best_params = None
+    def optimize_strategy(self, data):
+        results = [
+            self.run_backtest(short, long, data.copy()) 
+            for short in range(1, 20) 
+            for long in range(short + 1, 21)
+        ]
+        return sorted(results, key=lambda x: x['result']['Final Portfolio Value'], reverse=True)
     
-    for short_window in range(1, 20):
-        for long_window in range(short_window + 1, 21):  
-            strategy = MovingAverageStrategy(tickers=['AAPL'], interval='1d', short_window=short_window, long_window=long_window)
-            backtester = Backtester(strategy=strategy, initial_capital=100)
-            backtester.run(data)
-            result = backtester.get_results()
-
-            if best_result is None or result['Final Portfolio Value'] > best_result['Final Portfolio Value']:
-                best_result = result
-                best_params = (short_window, long_window)
-
-    return best_result, best_params
-
-
-def main():
-    data_with_signals = load_data()
+    def run_backtest(self, short_window, long_window, data):
+        strategy = MovingAverageStrategy(tickers=['AAPL'], interval='60m', short_window=short_window, long_window=long_window)
+        data_with_signals = strategy.generate_signals(data)
+        backtester = Backtester(strategy=strategy, initial_capital=self.initial_capital)
+        backtester.run(data_with_signals)
+        return {'params': (short_window, long_window), 'result': backtester.get_results()}
     
-    best_result, best_params = optimize_strategy(data_with_signals)
+    def print_results(self, sorted_results):
+        print("Top 5 Strategies:")
+        for res in sorted_results[:5]:
+            print(f"Parameters: {res['params']}, Final Portfolio Value: {res['result']['Final Portfolio Value']}")
+        
+        print("\nBottom 5 Strategies:")
+        for res in sorted_results[-5:]:
+            print(f"Parameters: {res['params']}, Final Portfolio Value: {res['result']['Final Portfolio Value']}")
     
-    print(f"Best Parameters: {best_params}")
-    print(f"Best Result: {best_result}")
-
-    strategy = MovingAverageStrategy(tickers=['AAPL'], interval='1d', short_window=best_params[0], long_window=best_params[1])
-    backtester = Backtester(strategy=strategy, initial_capital=100)
-    backtester.run(data_with_signals)
-    results = backtester.get_results()
-
-    print("Final Results with Best Parameters:")
-    print(results)
-
+    def main(self):
+        data = self.load_data('AAPL', '2023-01-01', '2023-12-31')
+        sorted_results = self.optimize_strategy(data)
+        self.print_results(sorted_results)
+        
+        # Run backtester again with best parameters
+        best_params = sorted_results[0]['params']
+        results = self.run_backtest(best_params[0], best_params[1], data.copy())['result']
+        
+        print("\nFinal Results with Best Parameters:")
+        print(f"Final Portfolio Value: {results['Final Portfolio Value']}")
+        
 if __name__ == "__main__":
-    main()
+    Main().main()
--- tests/test_backtester.py
+++ tests/test_backtester.py
@@ -6,78 +6,58 @@ import pandas as pd
 class TestBacktester(unittest.TestCase):
     
     def setUp(self):
-        self.strategy = MovingAverageStrategy(tickers=['AAPL'], interval='1d', short_window=2, long_window=3)
-        self.backtester = Backtester(strategy=self.strategy, initial_capital=100)
+        self.backtester = Backtester(strategy=None, initial_capital=100)
         
-        # Create a simple DataFrame with price data and precomputed buy/sell signals.
     data = pd.DataFrame({
         'price': [100, 101, 102, 103, 104],
-        'positions': [1, 0, -1, 1, 0]
+        'positions': [1, 0, -1, 0, 0]
     }, index=pd.date_range(start='2023-01-01', periods=5))
-
-
-    def test_buy_order(self):
+    
+    def test_run_with_buy_and_sell_order(self):
         self.backtester.run(self.data)
         results = self.backtester.get_results()
         
-        # Assuming transaction costs are not included
-        expected_cash = 100 - (100 * 1 * (1 + 0.001))  # Started with 100, bought one share at price 2
-        expected_position = 1  # One share bought
-        expected_portfolio_value = 103  # 98 cash + 1 share * price 5
-        
-        self.assertEqual(results['Final Cash'], expected_cash)
-        self.assertEqual(results['Final Position'], expected_position)
-        self.assertEqual(results['Final Portfolio Value'], expected_portfolio_value)
-        
-    def test_sell_order(self):
-        self.backtester.run(self.data)
-        results = self.backtester.get_results()
-        
-        # Assuming transaction costs are not included
-        expected_cash = (102 * 1 * (1 - 0.001))  # Sold one share at price 4
-        expected_position = 0  # All shares sold
+        expected_cash = 102  # After selling the share
+        expected_position = 0  # No shares, all are sold
         expected_portfolio_value = 102  # All in cash
-        
         self.assertEqual(results['Final Cash'], expected_cash)
         self.assertEqual(results['Final Position'], expected_position)
         self.assertEqual(results['Final Portfolio Value'], expected_portfolio_value)
         
-    def test_insufficient_cash(self):
-        self.backtester.cash = 1  # Set cash to an amount insufficient to buy a share
+    def test_run_with_insufficient_cash(self):
+        self.backtester.cash = 1
         self.backtester.run(self.data)
         results = self.backtester.get_results()
         
-        expected_cash = 1  # No share bought, so cash remains the same
-        expected_position = 0  # No share bought
+        expected_cash = 1  # Not enough cash to buy any share
+        expected_position = 0  # No shares were bought
         expected_portfolio_value = 1  # All in cash
-        
         self.assertEqual(results['Final Cash'], expected_cash)
         self.assertEqual(results['Final Position'], expected_position)
         self.assertEqual(results['Final Portfolio Value'], expected_portfolio_value)
         
-    def test_no_position_to_sell(self):
-        self.data.at[3, 'signal'] = -1  # Change to sell signal when no position is held
-        self.backtester.run(self.data)
+    def test_run_with_no_position_to_sell(self):
+        data_with_no_sell = self.data.copy()  # Create a copy to modify
+        data_with_no_sell.at['2023-01-03', 'positions'] = 0  # No sell signal, so it's kept until the end
+        
+        self.backtester.run(data_with_no_sell)  # Run with the modified data
         results = self.backtester.get_results()
         
-        expected_cash = 100  # No share sold, so cash remains the same
-        expected_position = 0  # No position to sell
-        expected_portfolio_value = 100  # All in cash
-        
+        expected_cash = 0  # All cash is used to buy 1 share
+        expected_position = 1  # 1 share is kept until the end
+        expected_portfolio_value = 104  # 1 share * 104 (last price)
         self.assertEqual(results['Final Cash'], expected_cash)
         self.assertEqual(results['Final Position'], expected_position)
         self.assertEqual(results['Final Portfolio Value'], expected_portfolio_value)
-        
-    def test_correct_transactions_recorded(self):
+
+    def test_transactions_recorded_correctly(self):
         self.backtester.run(self.data)
         results = self.backtester.get_results()
         
-        # Check if transactions are recorded correctly
         expected_transactions = [
             {'Date': 1, 'Type': 'BUY', 'Price': 100, 'Shares': 1},
             {'Date': 3, 'Type': 'SELL', 'Price': 102, 'Shares': 1}
         ]
-        
         self.assertEqual(results['Transactions'], expected_transactions)
 
 if __name__ == '__main__':
